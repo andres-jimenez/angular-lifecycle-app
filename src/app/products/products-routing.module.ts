cb3ac079ac1cb66784f0cab12466505bc907fe41
Andres Jimenez <andres.jimenez@pragma.com.co>
2023-05-17 09:15:22 -0500

    finish0

---
+++ src/app/products/products-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ProductsRoutingModule {}
